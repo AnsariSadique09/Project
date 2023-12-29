e07f4881c9390214b34cf9766e4c736be2a9235e
AnsariSadique09 <155127544+AnsariSadique09@users.noreply.github.com>
2023-12-29 09:19:12 +0530

    done

---
+++ script.js
@@ -0,0 +1,8 @@
+/* tying animation */
+
+var typed = new Typed(".typing",{
+    strings:["Web Desigener", "Web Developer", "Graphic Designer", "Youtuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
